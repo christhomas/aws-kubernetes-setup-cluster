5482828339aa3b6c482a606ebeaa38cc9661f830
Chris Thomas <chris.thomas@antimatter-studios.com>
2018-02-06 00:05:49 +0100

    move test-setup into the entry point and make the entire container fail if this script also fails
    Signed-off-by: Chris Thomas <chris.thomas@antimatter-studios.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env sh
+set -eu
+
+# Test the proper credentials and configuration are in place before attempting to execute anything
+test-setup
 
 export KOPS_STATE_STORE=s3://${BUCKET}
 
